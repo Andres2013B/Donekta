b6ffc618d214899fe1771cc2663bf5cdc0770461
Andres2013B <andresbraver@gmail.com>
2026-08-18 18:52:52 -0600

    Update image domain in next.config.js

--- donekta/next.config.js
+++ donekta/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['hssxfehpfihwbxfykkzj.supabase.co'],
+    domains: ['jzyvudqogfrpyzcnnoiv.supabase.co'],
   },
 }
 module.exports = nextConfig
