24a38acd571ab209908fcbe7129ffeca8c1d9e0d
Andres2013B <andresbraver@gmail.com>
2026-08-30 19:01:26 -0600

    Fix import path for checkDonorAccumulation

--- donekta/pages/api/pld/check.ts
+++ donekta/pages/api/pld/check.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { checkDonorAccumulation } from '../../../../lib/pld/check-donor-threshold'
+import { checkDonorAccumulation } from '../../../lib/pld/check-donor-threshold'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end()
